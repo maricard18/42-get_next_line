a527909b3a0a1b6666e786b2584ab66534db8c2b
Mario Cardoso Henriques <maricard@c1r6s6.42porto.com>
2022-11-30 15:34:02 +0000

    30 nov

---
+++ get_next_line/get_next_line.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: maricard <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/30 15:13:49 by maricard          #+#    #+#             */
+/*   Updated: 2022/11/30 15:28:47 by maricard         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef "GET_NEXT_LINE_H"
+# define "GET_NEXT_LINE_H"
+
+//C libraries
+# include <stdlib.h>
+
+//Function Prototypes
+char	*get_next_line(int fd);
+
+#endif
