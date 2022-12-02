1462d525a1f9157f4c6fc6ed319069fbf1e5c274
Mario Cardoso Henriques <maricard@c1r6s6.42porto.com>
2022-12-02 13:16:17 +0000

    next line

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -6,15 +6,91 @@
 /*   By: maricard <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/30 15:12:39 by maricard          #+#    #+#             */
-/*   Updated: 2022/11/30 15:33:20 by maricard         ###   ########.fr       */
+/*   Updated: 2022/12/02 13:06:54 by maricard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
+char	*ft_buf(int fd, char *stash)
+{
+	char	*buf;
+	char	*temp;
+	char	bytes_read;
+
+	buf = malloc(sizeof(char) * (BUFFER_SIZE + 1));
+	if (!buf)
+		return (0);
+	bytes_read = 1;
+	while (bytes_read > 0 && !ft_strchr(buf, '\n'))
+	{
+		bytes_read = read(fd, buf, BUFFER_SIZE);
+		if (!bytes_read)
+		{
+			free(buf);
+			free(stash);
+			return (0);
+		}
+		temp = ft_strjoin(stash, buf);
+		free(stash);
+		stash = temp;
+	}
+	return (stash);
+}
+
+char	*ft_get_line(stash)
+{
+	int		i;
+	int		a;
+	char	*line;
+
+	if (!stash)
+		return (0);
+	i = 0;
+	while (stash[i])
+	{
+		if (stash[i] == '\n')
+		{
+			line = malloc(sizeof(char) * (i + 1));
+			if (!line)
+				return (0);
+			break ;
+		}
+		i++;
+	}
+	a = 0;
+	while (a < i)
+		line[a++] = stash[a++];
+	return (line);
+}
+
 char	*get_next_line(int fd)
 {
-	int	i;
+	static char	*stash;
+	char		*line;
 
-	i = 0;
+	if (fd < 0 || BUFFER_SIZE <= 0)
+		return (0);
+	if (!stash)
+	{
+		stash = malloc(1);
+		stash[0] = '\0';
+	}
+	stash = ft_buf(fd, stash);
+	if (!stash)
+		return (0);
+	line = ft_get_line(stash);
+	return (line);
 }
+
+
+int	main(void)
+{
+	int	ola;
+
+	ola = open("teste", O_RDONLY);
+	printf("'%s'", get_next_line(ola));
+	printf("'%s'", get_next_line(ola));
+	return(0);
+}
+
--- get_next_line/get_next_line.h
+++ get_next_line/get_next_line.h
@@ -6,17 +6,31 @@
 /*   By: maricard <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/30 15:13:49 by maricard          #+#    #+#             */
-/*   Updated: 2022/11/30 15:28:47 by maricard         ###   ########.fr       */
+/*   Updated: 2022/12/02 13:15:49 by maricard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef "GET_NEXT_LINE_H"
-# define "GET_NEXT_LINE_H"
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
 
 //C libraries
 # include <stdlib.h>
+# include <unistd.h>
+# include <stdio.h>
+# include <fcntl.h>
+
+//Buffer size
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 42
+# endif
 
 //Function Prototypes
 char	*get_next_line(int fd);
+char	*ft_buf(int fd, char *stash);
+
+//Utility functions
+int		ft_strchr(char *buf, char c);
+char	*ft_strjoin(char *str1, char *str2);
+int		ft_strlen(char *str);
 
 #endif
